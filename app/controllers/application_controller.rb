75b29bab9a7411294eee03b1548ad51eba228f8d
Holden Altaffer <haltaffer@gmail.com>
2018-06-22 06:29:40 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
 
+  get '/recipes/new' do
+    erb :new
   end
 
   get '/recipes' do
@@ -23,9 +24,6 @@ class ApplicationController < Sinatra::Base
    erb :deleted
  end
 
-  get '/recipes/new' do
-    erb :new
-  end
 
   post '/recipes' do
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
